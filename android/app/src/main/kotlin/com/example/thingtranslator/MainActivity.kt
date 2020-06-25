62eb4ec8f17d1b52502fc008be09f99d049e15aa
MonipichMP <pichkhmer007@gmail.com>
2020-06-25 18:22:33 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/thingtranslator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.thingtranslator
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
